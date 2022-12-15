ea2604f6c6d63e788c53d4620ffa407cf2ce68a9
Igor Nikolayev <inikolaiev@access.dev>
2022-12-15 16:15:25 +0200

    add email sender

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,16 @@
 const {Router} = require('express');
 const User = require('../models/user')
-
 const router = Router();
+const nodeMailer = require('nodemailer');
+const nodeMailerSendGrid = require('nodemailer-sendgrid-transport');
 
+const mailAPIKey = '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';
+
+const transporter = nodeMailer.createTransport(nodeMailerSendGrid({
+    auth: {
+        api_key: mailAPIKey,
+    }
+}))
 router.get('/', (req, res) => {
     res.render('home');
 })
@@ -13,6 +21,14 @@ router.post('/', (req, res) => {
         phone: req.body.phone,
     });
     user.save();
+
+    transporter.sendMail({
+        to: 'inikolaiev@access.dev',
+        from: 'inikolaiev@access.dev',
+        subject: 'registration',
+        html: '<h2>Hello</h2>' +
+            '<p>Thank you for subscription</p>'
+    })
 
     res.redirect('/')
 })
