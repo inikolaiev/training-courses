793f0b930d05a1e271c99ad0b1838e27dac8de6e
Igor Nikolayev <inikolaiev@access.dev>
2022-12-15 17:07:11 +0200

    add validator.js

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 const router = Router();
 const nodeMailer = require('nodemailer');
 const nodeMailerSendGrid = require('nodemailer-sendgrid-transport');
-
+const {body, validationResult} = require('express-validator')
 const mailAPIKey = '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';
 
 const transporter = nodeMailer.createTransport(nodeMailerSendGrid({
@@ -15,11 +15,17 @@ router.get('/', (req, res) => {
     res.render('home');
 })
 
-router.post('/', (req, res) => {
+router.post('/', body('phone').isBoolean(), (req, res) => {
     const user = new User({
         name: req.body.name,
         phone: req.body.phone,
     });
+
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        console.log(error.array()[0].msg)
+        return res.status(422).redirect('/')
+    }
     user.save();
 
     transporter.sendMail({
